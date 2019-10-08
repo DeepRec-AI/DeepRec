4cfd44b26ca41eca1f040f2fc2124d17623a4e11
Frederic Bastien <fbastien@nvidia.com>
2019-10-08 12:00:28 -0700

    Add utiliry fct window_util::MakeWindow(sizes, strides)

--- tensorflow/compiler/xla/window_util_test.cc
+++ tensorflow/compiler/xla/window_util_test.cc
@@ -28,6 +28,14 @@ TEST(WindowUtilTest, HasOverlappingWindowTest) {
   EXPECT_TRUE(window_util::HasOverlappingWindow(window_util::MakeWindow({2,2,2,2})));
 }
 
+TEST(WindowUtilTest, MakeWindowStrideTest) {
+  // MakeWindow() set a stride of 1 by default.
+  Window w = window_util::MakeWindow({1, 2}, {3, 4});
+  EXPECT_EQ(w.dimensions()[0].size(), 1);
+  EXPECT_EQ(w.dimensions()[1].size(), 2);
+  EXPECT_EQ(w.dimensions()[0].stride(), 3);
+  EXPECT_EQ(w.dimensions()[1].stride(), 4);
+}
 
 }  // namespace
 }  // namespace xla
