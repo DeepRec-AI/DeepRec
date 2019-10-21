3bc19c43e81c12e9abbffa4f1097394539aef0ac
Yong Tang <yong.tang.github@outlook.com>
2019-10-21 14:39:33 +0000

    Fix build failures for python 3.8
    This fix tries to address the issue raised in 33543 where
    tensorflow build on python 3.8 failed.
    
    This fix fixed the issue as was suggested in 33543 and pip
    builds finished successfully.
    
    NOTE: tensorflow depends on h5py which does not have python 3.8
    support yet, as such a release version of tensorflow for python 3.8
    may have to wait for h5py first.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow/python/eager/pywrap_tfe_src.cc
+++ tensorflow/python/eager/pywrap_tfe_src.cc
@@ -1319,7 +1319,7 @@ static PyTypeObject TFE_Py_Tape_Type = {
     sizeof(TFE_Py_Tape),                          /* tp_basicsize */
     0,                                            /* tp_itemsize */
     &TFE_Py_Tape_Delete,                          /* tp_dealloc */
-    nullptr,                                      /* tp_print */
+    0,                                            /* tp_print */
     nullptr,                                      /* tp_getattr */
     nullptr,                                      /* tp_setattr */
     nullptr,                                      /* tp_reserved */
@@ -1353,7 +1353,7 @@ static PyTypeObject TFE_Py_ForwardAccumulator_Type = {
     sizeof(TFE_Py_ForwardAccumulator),                      /* tp_basicsize */
     0,                                                      /* tp_itemsize */
     &TFE_Py_ForwardAccumulatorDelete,                       /* tp_dealloc */
-    nullptr,                                                /* tp_print */
+    0,                                                      /* tp_print */
     nullptr,                                                /* tp_getattr */
     nullptr,                                                /* tp_setattr */
     nullptr,                                                /* tp_reserved */
