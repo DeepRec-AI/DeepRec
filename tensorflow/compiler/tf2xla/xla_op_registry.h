5143a097fef7ff0aa3040b37764d5c157336ae04
Agoniii <815244047@qq.com>
2019-12-02 20:04:00 +0800

    using std::string

--- tensorflow/compiler/tf2xla/xla_op_registry.h
+++ tensorflow/compiler/tf2xla/xla_op_registry.h
@@ -267,7 +267,7 @@ class XlaOpRegistry {
     // operands and not their values.
     bool is_metadata_op = false;
 
-    string label;
+    std::string label;
 
     // Factory used to build OpKernels that perform symbolic execution.
     Factory factory;
@@ -350,7 +350,7 @@ class XlaOpRegistrationBuilder {
   XlaOpRegistrationBuilder& IsMetadataOp();
 
   // Specifies a particular value for the "_kernel" attr.  
-  XlaOpRegistrationBuilder& Label(absl::string_view label);
+  XlaOpRegistrationBuilder& Label(std::string label);
 
   std::unique_ptr<XlaOpRegistry::OpRegistration> Build(
       XlaOpRegistry::Factory factory);
