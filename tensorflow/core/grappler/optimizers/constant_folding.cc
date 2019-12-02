505aab96c4fbccafd0258dbeb7ca1ad1b781a29a
Jhalak <jhalakp@nvidia.com>
2019-12-02 14:58:42 -0800

    Fix FakeQuantOps constant folding

--- tensorflow/core/grappler/optimizers/constant_folding.cc
+++ tensorflow/core/grappler/optimizers/constant_folding.cc
@@ -961,7 +961,7 @@ bool ConstantFolding::IsFoldable(const NodeDef& node,
       op.find("Reader") != string::npos) {
     return false;
   }
-  if (op.find("Quantized") != string::npos || op.find("Sparse") == 0) {
+  if (op.find("Quant") != string::npos || op.find("Sparse") == 0) {
     return false;
   }
 
