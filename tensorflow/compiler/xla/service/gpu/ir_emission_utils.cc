4e240cfebddc638efcb7792228297fcd6e0c38c3
Trent Lo <trentl@nvidia.com>
2019-12-05 11:01:21 -0800

    Implement slice input fusion.

--- tensorflow/compiler/xla/service/gpu/ir_emission_utils.cc
+++ tensorflow/compiler/xla/service/gpu/ir_emission_utils.cc
@@ -225,6 +225,37 @@ bool IsReductionFromOrToContiguousDimensions(const HloInstruction& reduce) {
   return dims_in_elem[1] >= kWarpSize;
 }
 
+bool IsInputFusibleSlices(const HloInstruction& unnested_hlo,
+                          bool verify_no_strides) {
+  if (!unnested_hlo.IsInputFusion()) {
+    return false;
+  }
+
+  auto is_non_strided = [](const std::vector<int64>& strides) -> bool {
+    return absl::c_all_of(strides, [](int stride) { return stride == 1; });
+  };
+
+  const auto root = unnested_hlo.fused_expression_root();
+  if (root->opcode() == HloOpcode::kSlice) {
+    return !verify_no_strides || is_non_strided(root->slice_strides());
+  }
+
+  if (root->opcode() != HloOpcode::kTuple) {
+    return false;
+  }
+
+  bool all_non_strided_slices =
+      absl::c_all_of(root->operands(), [&](const HloInstruction* insn) {
+        return insn->opcode() == HloOpcode::kSlice &&
+               (!verify_no_strides || is_non_strided(insn->slice_strides()));
+      });
+  if (!all_non_strided_slices) {
+    return false;
+  }
+
+  return true;
+}
+
 std::pair<bool, DimensionVector> GetReductionKindAndContiguousComponents(
     const Shape& input_shape, absl::Span<const int64> dims_to_reduce) {
   DimensionVector dims_to_keep;
