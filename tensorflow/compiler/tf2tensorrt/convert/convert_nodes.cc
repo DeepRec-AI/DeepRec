be49df4c49070797a9fe2cfe5d0966b4da15dc0e
Pooya Davoodi <pdavoodi@nvidia.com>
2019-12-17 16:57:50 -0800

    Add additional padding layer to deconv converter to fix output_shape
    This becomes only effective if stride>1.

--- tensorflow/compiler/tf2tensorrt/convert/convert_nodes.cc
+++ tensorflow/compiler/tf2tensorrt/convert/convert_nodes.cc
@@ -2095,8 +2095,25 @@ Status ConvertConv2DHelper(OpConverterParams* params, int group,
     layer->setDilation(dilation);
     conv_layer = layer;
   }
-  nvinfer1::ITensor* output_tensor = conv_layer->getOutput(0);
-
+  nvinfer1::ITensor* conv_output_tensor = conv_layer->getOutput(0);
+  nvinfer1::ITensor* output_tensor;
+  // Add an extra padding in case of Deconv because TRT doesn't accept
+  // argument output_shape and thus the TRT output shape could be wrong
+  // in case of strides>1.
+  if (is_conv2d_backprop_input) {
+    auto trt_dims = backprop_output_size.GetTrtDims();
+    nvinfer1::Dims conv_output_dims = conv_output_tensor->getDimensions();
+    const int heightDiff = trt_dims.d[h_index - 1] - conv_output_dims.d[1];
+    const int widthDiff = trt_dims.d[w_index - 1] - conv_output_dims.d[2];
+    nvinfer1::DimsHW pre_padding(0, 0);
+    nvinfer1::DimsHW post_padding(heightDiff, widthDiff);
+    nvinfer1::IPaddingLayer* padding_layer = params->converter->network()->addPadding(
+        *conv_output_tensor, pre_padding, post_padding);
+    output_tensor = padding_layer->getOutput(0);
+  }
+  else {
+    output_tensor = conv_output_tensor;
+  }
   // Restore transpose.
   if (need_transpose) {
     TF_RETURN_IF_ERROR(params->converter->TransposeTensor(
--- tensorflow/python/compiler/tensorrt/test/conv2d_test.py
+++ tensorflow/python/compiler/tensorrt/test/conv2d_test.py
@@ -169,7 +169,7 @@ class Conv2DStridedNCHWTest(trt_test.TfTrtIntegrationTestBase):
     return ["TRTEngineOp_0"]
 
 
-class Conv2DTranposeTest(trt_test.TfTrtIntegrationTestBase):
+class Conv2DTranposeSAMEPaddingTest(trt_test.TfTrtIntegrationTestBase):
   """Testing conversion of conv2d_transpose (AKA Conv2DBackpropInput)"""
 
   def GraphFn(self, inp):
@@ -193,6 +193,36 @@ class Conv2DTranposeTest(trt_test.TfTrtIntegrationTestBase):
   def GetParams(self):
     return self.BuildParams(self.GraphFn, dtypes.float32, [[13, 3, 7, 11]],
                             [[13, 8, 14, 22]])
+
+  def ExpectedEnginesToBuild(self, run_params):
+    """Return the expected engines to build."""
+    return ["TRTEngineOp_0"]
+
+
+class Conv2DTranposeVALIDPaddingTest(trt_test.TfTrtIntegrationTestBase):
+  """Testing conversion of conv2d_transpose (AKA Conv2DBackpropInput)"""
+
+  def GraphFn(self, inp):
+    np.random.seed(1234)
+    dtype = inp.dtype
+    n, c, h, w = 13, 3, 7, 11
+    num_filters = 8
+    weights_shape = [2, 2, num_filters, c]
+    weights = constant_op.constant(np.random.randn(*weights_shape), dtype=dtype)
+    output_shape = constant_op.constant([n, num_filters, h * 2 + 1, w * 2],
+                                        dtype=dtypes.int32)
+    output = nn_ops.conv2d_transpose(
+        inp,
+        weights,
+        output_shape,
+        strides=[1, 1, 2, 2],
+        padding="VALID",
+        data_format="NCHW")
+    return array_ops.identity(output, name="output_0")
+
+  def GetParams(self):
+    return self.BuildParams(self.GraphFn, dtypes.float32, [[13, 3, 7, 11]],
+                            [[13, 8, 15, 22]])
 
   def ExpectedEnginesToBuild(self, run_params):
     """Return the expected engines to build."""
