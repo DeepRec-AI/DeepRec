706ba7e2685b2ded84470b7e7e47f05fab0eabb3
Frederic Bastien <fbastien@nvidia.com>
2020-06-18 11:42:29 -0700

    Continue TF API bridge

--- tensorflow/stream_executor/gpu/asm_compiler.h
+++ tensorflow/stream_executor/gpu/asm_compiler.h
@@ -1,12 +1,18 @@
+#include "tensorflow/stream_executor/cuda/ptxas_utils.h"
+
 #ifndef TENSORFLOW_STREAM_EXECUTOR_GPU_ASM_COMPILER_H_
 #define TENSORFLOW_STREAM_EXECUTOR_GPU_ASM_COMPILER_H_
 
 namespace stream_executor {
-
-  port::StatusOr<std::vector<uint8>> CompileGpuAsm(int device_ordinal,
+port::StatusOr<std::vector<uint8>> CompileGpuAsm(int device_ordinal,
                                                  const char* ptx_contents,
                                                  GpuAsmOpts options) {
   return CompilePtx(device_ordinal, ptx_contents, options);
+}
+
+port::StatusOr<absl::Span<const uint8>> CompileGpuAsmOrGetCached(
+    int device_ordinal, const char* ptx, GpuAsmOpts compilation_options) {
+  return CompilePtxOrGetCached(device_ordinal, ptx, compilation_options);
 }
 
 }  // namespace stream_executor
