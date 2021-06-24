DEKHTIARJonathan/tensorflow

Fixed TF32 related tolerance errors in the following tests.
tensordot tests
einsum tests
mat square tests
self adjoint eig tests
eig tests
matrix exponential tests
cholesky tests
matmul_op_test

tensorflow/python/kernel_tests/einsum_op_test.py
@@ -172,7 +172,8 @@ class EinsumOpTest(test.TestCase):
       else:
         a = np.einsum(equation, *inputs)
 
-      b = self.evaluate(gen_linalg_ops.einsum(input_tensors, equation))
+      with ops.device("/cpu:0"):
+        b = self.evaluate(gen_linalg_ops.einsum(input_tensors, equation))
       tol = 1e-2 if dtype == bfloat16 else 1e-4
       self.assertAllClose(a, b, atol=tol, rtol=tol)
 

DeepRec-AI/DeepRec

tensorflow/python/kernel_tests/einsum_op_test.py
@@ -46,7 +46,8 @@ class EinsumOpTest(test.TestCase):
       inputs.append(arr)
     input_tensors = [constant_op.constant(x, shape=x.shape) for x in inputs]
     a = np.einsum(s, *inputs)
-    b = self.evaluate(gen_linalg_ops.einsum(input_tensors, s))
+    with ops.device("/cpu:0"):
+      b = self.evaluate(gen_linalg_ops.einsum(input_tensors, s))
     self.assertAllClose(a, b, atol=1e-4, rtol=1e-4)
 
   def testUnary(self):