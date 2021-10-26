4570e67484690c6fe7f883e7ae3a212ec717a57f
Nathan Luehr <nluehr@nvidia.com>
2021-10-26 08:41:23 -0700

    Use CUB from the CUDA Toolkit starting with version 11.0.
    Backport of upstream commit c6769e20b.

--- tensorflow/core/kernels/topk_op_gpu.h
+++ tensorflow/core/kernels/topk_op_gpu.h
@@ -23,9 +23,9 @@ limitations under the License.
 #include <vector>
 
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
-#include "third_party/cub/device/device_segmented_radix_sort.cuh"
-#include "third_party/cub/iterator/counting_input_iterator.cuh"
-#include "third_party/cub/iterator/transform_input_iterator.cuh"
+#include "cub/device/device_segmented_radix_sort.cuh"
+#include "cub/iterator/counting_input_iterator.cuh"
+#include "cub/iterator/transform_input_iterator.cuh"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/framework/tensor.h"
