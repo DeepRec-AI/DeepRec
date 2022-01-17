ce25f9c643fb6830e8177877b9a7d62c0bfc1906
tongxuan.ltx <tongxuan.ltx@alibaba-inc.com>
2022-01-17 16:47:43 +0800

    [Refactor] Refactoring code style of incr_save_restore_ops.

--- tensorflow/core/kernels/incr_save_restore_ops.h
+++ tensorflow/core/kernels/incr_save_restore_ops.h
@@ -12,6 +12,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#ifndef TENSORFLOW_CORE_KERNELS_INCR_SAVE_RESTORE_OPS_H_
+#define TENSORFLOW_CORE_KERNELS_INCR_SAVE_RESTORE_OPS_H_
 
 #include <string>
 #include <vector>
@@ -37,14 +39,12 @@ limitations under the License.
 #include "tensorflow/core/lib/strings/stringprintf.h"
 
 namespace tensorflow {
-
-
-template <typename T> 
+template <typename T>
 class ThreadSafeHashMap {
  public:
   ThreadSafeHashMap() {}
   ~ThreadSafeHashMap() {}
-  
+
  public:
   void Update(const Tensor& indices, int64 start, int64 end) {
     mutex_lock l(lock_);
@@ -60,12 +60,12 @@ class ThreadSafeHashMap {
     }
   }
 
-  void Swap(std::unordered_map<T, uint64> &out) {
+  void Swap(std::unordered_map<T, uint64>& out) {
     mutex_lock l(lock_);
     hash_map_.swap(out);
   }
 
-  void GetKeys(std::set<T> &key_set) {
+  void GetKeys(std::set<T>& key_set) {
     mutex_lock l(lock_);
     for (auto it : hash_map_) {
       key_set.insert(it.first);
@@ -76,6 +76,7 @@ class ThreadSafeHashMap {
     mutex_lock l(lock_);
     hash_map_.clear();
   }
+
  private:
   std::unordered_map<T, uint64> hash_map_;
   mutex lock_;
@@ -84,19 +85,19 @@ class ThreadSafeHashMap {
 template <typename T>
 class ParallelHashMap {
  public:
-  ParallelHashMap(int min_part_size = 128, int part_count = 32) 
-      : part_count_(part_count) 
-      , min_part_size_(min_part_size)
-  {
+  explicit ParallelHashMap(int min_part_size = 128, int part_count = 32)
+      : part_count_(part_count),
+      min_part_size_(min_part_size) {
     hash_maps_.resize(part_count_);
   }
 
   void Update(const Tensor& indices, OpKernelContext *ctx) {
     const int64 N = indices.NumElements();
     auto thread_pool = *(ctx->device()->tensorflow_cpu_worker_threads());
-        
+
     std::vector<std::pair<int64, int64>> parts;
-    SplitParallelParts(N, std::min(part_count_, thread_pool.workers->NumThreads()), parts);
+    SplitParallelParts(N,
+        std::min(part_count_, thread_pool.workers->NumThreads()), parts);
 
     int part_count = parts.size();
     BlockingCounter counter(part_count);
@@ -117,7 +118,7 @@ class ParallelHashMap {
     for (int i = 0; i < part_count_; i++) {
       hash_maps_[i].Swap(tmp_maps[i]);
     }
-        
+
     indices.clear();
     for (int i = 0; i < part_count_; i++) {
       for (auto it : tmp_maps[i]) {
@@ -137,15 +138,14 @@ class ParallelHashMap {
     }
   }
 
-  void GetKeys(std::set<T> &key_set) {
+  void GetKeys(std::set<T>& key_set) {
     for (size_t i = 0; i < part_count_; i++) {
       hash_maps_[i].GetKeys(key_set);
     }
   }
 
-  void SplitParallelParts(int64 total_num, int64 part_count, 
-                          std::vector<std::pair<int64, int64>> &parts) 
-  {
+  void SplitParallelParts(int64 total_num, int64 part_count,
+      std::vector<std::pair<int64, int64>>& parts) {
     if (total_num == 0) {
       return;
     }
@@ -155,8 +155,8 @@ class ParallelHashMap {
     if (part_size < min_part_size_) {
       actual_part_count = total_num / min_part_size_;
       actual_part_count = actual_part_count == 0 ? 1 : actual_part_count;
-    } 
-        
+    }
+
     part_size = total_num / actual_part_count;
     int64 left = total_num % actual_part_count;
     int64 start = 0;
@@ -177,17 +177,19 @@ class ParallelHashMap {
 template <class K>
 class IncrKeyDumpIterator : public DumpIterator<K> {
  public:
-  IncrKeyDumpIterator(std::vector<K>& incr_keys):incr_keys_(incr_keys)  {
+  explicit IncrKeyDumpIterator(std::vector<K>& incr_keys)
+    : incr_keys_(incr_keys) {
     keys_iter_  = incr_keys_.begin();
   }
 
   bool HasNext() const {
-    return keys_iter_ != incr_keys_.end(); 
+    return keys_iter_ != incr_keys_.end();
   }
 
   K Next() {
     return *keys_iter_++;
   }
+
  private:
   std::vector<K>& incr_keys_;
   typename std::vector<K>::iterator keys_iter_;
@@ -196,7 +198,10 @@ class IncrKeyDumpIterator : public DumpIterator<K> {
 template<class K, class T>
 class IncrEVValueDumpIterator : public  DumpIterator<T> {
  public:
-  IncrEVValueDumpIterator(std::vector<K>& incr_keys, EmbeddingVar<K, T>*& emb_var):incr_keys_(incr_keys), emb_var_(emb_var){
+  IncrEVValueDumpIterator(std::vector<K>& incr_keys,
+      EmbeddingVar<K, T>*& emb_var)
+    : incr_keys_(incr_keys),
+    emb_var_(emb_var) {
     keys_iter_ = incr_keys_.begin();
     keys_idx_ = 1;
     col_idx_ = 0;
@@ -204,12 +209,14 @@ class IncrEVValueDumpIterator : public  DumpIterator<T> {
 
   bool HasNext() const {
     if (keys_iter_ != incr_keys_.end()) {
-      if (keys_idx_ < incr_keys_.size())
+      if (keys_idx_ < incr_keys_.size()) {
         return true;
-      else
+      } else {
         return col_idx_ < emb_var_->ValueLen();
-    } else
+      }
+    } else {
       return false;
+    }
   }
 
   T Next() {
@@ -234,11 +241,14 @@ class IncrEVValueDumpIterator : public  DumpIterator<T> {
 template<class K, class V, class T>
 class IncrEVVersionDumpIterator : public  DumpIterator<T> {
  public:
-  IncrEVVersionDumpIterator(std::vector<K>& incr_keys, EmbeddingVar<K, V>*& emb_var):incr_keys_(incr_keys), emb_var_(emb_var){
+  IncrEVVersionDumpIterator(std::vector<K>& incr_keys,
+      EmbeddingVar<K, V>*& emb_var) :
+    incr_keys_(incr_keys),
+    emb_var_(emb_var) {
     keys_iter_ = incr_keys_.begin();
   }
 
-  bool HasNext () const {
+  bool HasNext() const {
     return keys_iter_ != incr_keys_.end();
   }
 
@@ -263,7 +273,10 @@ class IncrEVVersionDumpIterator : public  DumpIterator<T> {
 template<class K, class V, class T>
 class IncrEVFreqDumpIterator : public  DumpIterator<T> {
  public:
-  IncrEVFreqDumpIterator(std::vector<K>& incr_keys, EmbeddingVar<K, V>*& emb_var):incr_keys_(incr_keys), emb_var_(emb_var){
+  IncrEVFreqDumpIterator(std::vector<K>& incr_keys,
+      EmbeddingVar<K, V>*& emb_var)
+    : incr_keys_(incr_keys),
+    emb_var_(emb_var) {
     keys_iter_ = incr_keys_.begin();
   }
 
@@ -287,7 +300,10 @@ class IncrEVFreqDumpIterator : public  DumpIterator<T> {
 template<class K, class T>
 class IncrNormalValueDumpIterator : public  DumpIterator<T> {
  public:
-  IncrNormalValueDumpIterator(std::vector<K>& incr_keys, const Tensor& variable):incr_keys_(incr_keys), variable_(variable){
+  IncrNormalValueDumpIterator(std::vector<K>& incr_keys,
+      const Tensor& variable)
+    : incr_keys_(incr_keys),
+    variable_(variable) {
     var_data_ = (T*)variable.flat<T>().data();
     keys_iter_ = incr_keys_.begin();
     keys_idx_ = 1;
@@ -296,12 +312,14 @@ class IncrNormalValueDumpIterator : public  DumpIterator<T> {
 
   bool HasNext() const {
     if (keys_iter_ != incr_keys_.end()) {
-      if (keys_idx_ < incr_keys_.size())
+      if (keys_idx_ < incr_keys_.size()) {
         return true;
-      else 
+      } else {
         return col_idx_ < variable_.dim_size(1);
-    } else
+      }
+    } else {
       return false;
+    }
   }
 
   T Next() {
@@ -328,19 +346,16 @@ class IncrNormalValueDumpIterator : public  DumpIterator<T> {
 template <typename K, typename V = float>
 class IndicesIncrRecorder: public ResourceBase {
  public:
-  explicit IndicesIncrRecorder(const std::string &name, 
-                               int32 part_count = 16,
-                               int32 min_part_size = 128) 
-      : name_(name)
-      , incr_indices_(min_part_size, part_count)
-  {}
-
-  ~IndicesIncrRecorder() override {}
+  explicit IndicesIncrRecorder(const std::string &name,
+      int32 part_count = 16, int32 min_part_size = 128)
+      : name_(name),
+      incr_indices_(min_part_size, part_count) {}
 
   void UpdateIndices(const Tensor& indices, OpKernelContext *ctx) {
-    if (global_version_ == -1)
+    if (global_version_ == -1) {
       return;
-    
+    }
+
     incr_indices_.Update(indices, ctx);
   }
 
@@ -350,37 +365,35 @@ class IndicesIncrRecorder: public ResourceBase {
     incr_indices_.Clear();
   }
 
-  void SwapIndices(std::unordered_map<K, uint64> &indices) {
+  void SwapIndices(std::unordered_map<K, uint64>& indices) {
     incr_indices_.Swap(indices);
   }
 
-  Status DumpSparseNormalTensor(const string& tensor_name, const Tensor& variable, BundleWriter* writer) {
+  Status DumpSparseNormalTensor(const string& tensor_name,
+      const Tensor& variable, BundleWriter* writer) {
     mutex_lock l(mu_);
     size_t bytes_limit = 8 << 20;
     char* dump_buffer = (char*)malloc(sizeof(char) * bytes_limit);
-    Status st;
 
     std::set<K> incr_keys_set;
     incr_indices_.GetKeys(incr_keys_set);
     std::vector<K> incr_keys;
     incr_keys.assign(incr_keys_set.begin(), incr_keys_set.end());
 
-   
     IncrKeyDumpIterator<K> key_dump_iter(incr_keys);
-    st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_keys", 
-                                   writer, dump_buffer, bytes_limit, 
-                                   &key_dump_iter, 
-                                   TensorShape({incr_keys.size()}));
+    Status st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_keys",
+        writer, dump_buffer, bytes_limit, &key_dump_iter,
+        TensorShape({incr_keys.size()}));
     if (!st.ok()) {
       free(dump_buffer);
       return st;
     }
 
     IncrNormalValueDumpIterator<K, V> value_dump_iter(incr_keys, variable);
-    st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_values", 
-                                   writer, dump_buffer, bytes_limit, 
-                                   &value_dump_iter, 
-                                   TensorShape({incr_keys.size(), variable.dim_size(1)}));
+    st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_values",
+        writer, dump_buffer, bytes_limit,
+        &value_dump_iter,
+        TensorShape({incr_keys.size(), variable.dim_size(1)}));
     if (!st.ok()) {
       free(dump_buffer);
       return st;
@@ -390,21 +403,23 @@ class IndicesIncrRecorder: public ResourceBase {
     return Status::OK();
   }
 
-  Status DumpSparseEmbeddingTensor(const string& tensor_name, EmbeddingVar<K, V>* emb_var, BundleWriter* writer, OpKernelContext* context) {
+  Status DumpSparseEmbeddingTensor(const string& tensor_name,
+      EmbeddingVar<K, V>* emb_var, BundleWriter* writer,
+      OpKernelContext* context) {
     mutex_lock l(mu_);
     size_t bytes_limit = 8 << 20;
     char* dump_buffer = (char*)malloc(sizeof(char) * bytes_limit);
-    Status st;
 
     std::set<K> incr_keys;
     incr_indices_.GetKeys(incr_keys);
 
     std::vector<std::vector<K> > incr_keys_parts;
     incr_keys_parts.resize(kSavedPartitionNum);
- 
-    for (auto& ik: incr_keys) {
+
+    for (auto& ik : incr_keys) {
       for (int partid = 0; partid < kSavedPartitionNum; partid++) {
-        if (ik % kSavedPartitionNum == partid && emb_var->GetFreq(ik) >= emb_var->MinFreq()) {
+        if (ik % kSavedPartitionNum == partid &&
+            emb_var->GetFreq(ik) >= emb_var->MinFreq()) {
           incr_keys_parts[partid].push_back(ik);
           break;
         }
@@ -413,7 +428,8 @@ class IndicesIncrRecorder: public ResourceBase {
 
     std::vector<K> partitioned_incr_keys;
     Tensor part_offset_tensor;
-    context->allocate_temp(DT_INT32, TensorShape({kSavedPartitionNum + 1}), &part_offset_tensor);
+    context->allocate_temp(DT_INT32,
+        TensorShape({kSavedPartitionNum + 1}), &part_offset_tensor);
     auto part_offset_flat = part_offset_tensor.flat<int32>();
     part_offset_flat(0) = 0;
     int ptsize = 0;
@@ -429,42 +445,40 @@ class IndicesIncrRecorder: public ResourceBase {
     }
     writer->Add(tensor_name+ "-incr_partition_offset", part_offset_tensor);
 
-
-
     IncrKeyDumpIterator<K> key_dump_iter(partitioned_incr_keys);
-    st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_keys",
-                                   writer, dump_buffer, bytes_limit,
-                                   &key_dump_iter,
-                                   TensorShape({partitioned_incr_keys.size()}));
+    Status st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_keys",
+        writer, dump_buffer, bytes_limit, &key_dump_iter,
+        TensorShape({partitioned_incr_keys.size()}));
     if (!st.ok()) {
       free(dump_buffer);
       return st;
     }
-     
-    IncrEVValueDumpIterator<K, V> ev_value_dump_iter(partitioned_incr_keys, emb_var);
+
+    IncrEVValueDumpIterator<K, V> ev_value_dump_iter(
+        partitioned_incr_keys, emb_var);
     st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_values",
-                                   writer, dump_buffer, bytes_limit,
-                                   &ev_value_dump_iter,
-                                   TensorShape({(uint64)partitioned_incr_keys.size(), emb_var->ValueLen()}));
+        writer, dump_buffer, bytes_limit, &ev_value_dump_iter,
+        TensorShape({(uint64)partitioned_incr_keys.size(),
+          emb_var->ValueLen()}));
     if (!st.ok()) {
       free(dump_buffer);
       return st;
     }
-   
-    IncrEVVersionDumpIterator<K, V, int64> ev_version_dump_iter(partitioned_incr_keys, emb_var);
+
+    IncrEVVersionDumpIterator<K, V, int64> ev_version_dump_iter(
+        partitioned_incr_keys, emb_var);
     st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_versions",
-                                   writer, dump_buffer, bytes_limit,
-                                   &ev_version_dump_iter,
-                                   TensorShape({(uint64)partitioned_incr_keys.size()}));
+        writer, dump_buffer, bytes_limit, &ev_version_dump_iter,
+        TensorShape({(uint64)partitioned_incr_keys.size()}));
     if (!st.ok()) {
       free(dump_buffer);
       return st;
     }
-    IncrEVFreqDumpIterator<K, V, int64> ev_freq_dump_iter(partitioned_incr_keys, emb_var);
+    IncrEVFreqDumpIterator<K, V, int64> ev_freq_dump_iter(
+        partitioned_incr_keys, emb_var);
     st = SaveTensorWithFixedBuffer(tensor_name + "-sparse_incr_freqs",
-                                   writer, dump_buffer, bytes_limit,
-                                   &ev_freq_dump_iter,
-                                   TensorShape({(uint64)partitioned_incr_keys.size()}));
+        writer, dump_buffer, bytes_limit, &ev_freq_dump_iter,
+        TensorShape({(uint64)partitioned_incr_keys.size()}));
     if (!st.ok()) {
       free(dump_buffer);
       return st;
@@ -473,8 +487,6 @@ class IndicesIncrRecorder: public ResourceBase {
     return Status::OK();
   }
 
-
-  
   string DebugString() const {
     return "IndicesIncrRecorder";
   }
@@ -496,20 +508,20 @@ class SparsePartitioner {
  public:
   SparsePartitioner(int64 part_count, int64_t part_idx,
                     int64 hash_bucket_size)
-      : part_count_(part_count)
-      , part_idx_(part_idx)
-      , hash_bucket_size_(hash_bucket_size)
-  {
+      : part_count_(part_count),
+      part_idx_(part_idx),
+      hash_bucket_size_(hash_bucket_size) {
     assert(part_idx_ >= part_count_);
   }
 
   virtual int64 CalcGlobalOffset(int64 part_offset) = 0;
 
   std::string toString() const {
-    return strings::Printf("part_mode:%s, part_count:%lld, part_idx:%ld, hash_bucket_size:%ld",
-                           part_mode_.c_str(), part_count_, part_idx_, (long)hash_bucket_size_);
+    return strings::Printf(
+        "part_mode:%s, part_count:%lld, part_idx:%ld, hash_bucket_size:%ld",
+        part_mode_.c_str(), part_count_, part_idx_, (long)hash_bucket_size_);
   }
-    
+
  protected:
   std::string part_mode_;
   int64 part_count_;
@@ -521,10 +533,8 @@ class DivSparsePartitioner : public SparsePartitioner {
  public:
   DivSparsePartitioner(int64 part_count, int64 part_idx,
                        int64 hash_bucket_size)
-      : SparsePartitioner(part_count, part_idx, hash_bucket_size)
-  {
+      : SparsePartitioner(part_count, part_idx, hash_bucket_size) {
     part_mode_ = "div";
-
     int64 ids_per_part = hash_bucket_size_ / part_count_;
     int64 extras = hash_bucket_size_ % part_count_;
 
@@ -533,7 +543,7 @@ class DivSparsePartitioner : public SparsePartitioner {
       part_offset_start_ += (i < extras ? (ids_per_part + 1) : ids_per_part);
     }
   }
-    
+
   int64 CalcGlobalOffset(int64 part_offset) {
     return part_offset_start_ + part_offset;
   }
@@ -545,8 +555,7 @@ class ModSparsePartitioner : public SparsePartitioner {
  public:
   ModSparsePartitioner(int64 part_count, int64 part_idx,
                        int64 hash_bucket_size)
-      : SparsePartitioner(part_count, part_idx, hash_bucket_size)
-  {
+      : SparsePartitioner(part_count, part_idx, hash_bucket_size) {
     part_mode_ = "mod";
   }
 
@@ -556,3 +565,5 @@ class ModSparsePartitioner : public SparsePartitioner {
 };
 
 } // namespace tensorflow
+
+#endif // TENSORFLOW_CORE_KERNELS_INCR_SAVE_RESTORE_OPS_H_
