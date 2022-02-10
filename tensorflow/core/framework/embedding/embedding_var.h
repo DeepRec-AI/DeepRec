lixy9474/DeepRec-1

[Embedding] Support Contious ValuePtr.

tensorflow/core/framework/embedding/embedding_var.h
@@ -100,6 +100,9 @@ class EmbeddingVar : public ResourceBase {
       default_value_ = TypedAllocator::Allocate<V>(alloc_, default_tensor.NumElements(), AllocationAttributes());
       auto default_tensor_flat = default_tensor.flat<V>();
       memcpy(default_value_, &default_tensor_flat(0), default_tensor.TotalBytes());
+      if (LayoutType::NORMAL_FIX == emb_config_.get_layout_type()) {
+        kv_->SetDim(emb_config_.emb_index, value_len_, emb_config_.slot_num + 1);
+      }
       return Status::OK();
     }
   }
@@ -131,6 +134,8 @@ class EmbeddingVar : public ResourceBase {
       }
       new_value_ptr_fn = [this] (size_t size) { return new DBValuePtr<V>(size, this->level_db_); };
      }
+    } else if (LayoutType::NORMAL_FIX == emb_config_.get_layout_type()){
+      new_value_ptr_fn = [] (size_t size) { return new NormalContinousValuePtr<V>(size); };
     } else {
       return errors::InvalidArgument(name_, ", Unsupport EmbeddingVariable LayoutType.");
     }
@@ -163,6 +168,9 @@ class EmbeddingVar : public ResourceBase {
       default_value_ = TypedAllocator::Allocate<V>(alloc_, default_tensor.NumElements(), AllocationAttributes());
       auto default_tensor_flat = default_tensor.flat<V>();
       memcpy(default_value_, &default_tensor_flat(0), default_tensor.TotalBytes());
+      if (LayoutType::NORMAL_FIX == emb_config_.get_layout_type()) {
+        kv_->SetDim(emb_config_.emb_index, value_len_, emb_config_.slot_num + 1);
+      }
       return Status::OK();
     }
   }
@@ -180,7 +188,7 @@ class EmbeddingVar : public ResourceBase {
   }
 
   Status LookupOrCreateKey(K key, ValuePtr<V>** value_ptr, int64 update_version = -1) {
-    Status s = LookupOrCreateKeyInternal(key, value_ptr, emb_config_.total_num());
+    Status s = LookupOrCreateKeyInternal(key, value_ptr, emb_config_.total_num(kv_->GetTotalDims()));
     TF_CHECK_OK(s);
     if (emb_config_.is_primary() && emb_config_.steps_to_live != 0 && update_version != -1) {
       (*value_ptr)->SetStep(update_version);
@@ -210,11 +218,11 @@ class EmbeddingVar : public ResourceBase {
 
   V* LookupOrCreateEmb(ValuePtr<V>* value_ptr, const V* default_v) {
     return value_ptr->GetOrAllocate(alloc_, value_len_, default_v,
-        emb_config_.emb_index);
+        emb_config_.emb_index, kv_->GetOffset(emb_config_.emb_index));
   }
 
   V* LookupPrimaryEmb(ValuePtr<V>* value_ptr) {
-    V* primary_val = value_ptr->GetValue(emb_config_.primary_emb_index, value_len_);
+    V* primary_val = value_ptr->GetValue(emb_config_.primary_emb_index, value_len_, kv_->GetOffset(emb_config_.emb_index));
     return primary_val;
   }
 
@@ -297,8 +305,8 @@ class EmbeddingVar : public ResourceBase {
     std::vector<K> key_list_tmp;
     kv_->GetSnapshot(&key_list_tmp, &value_ptr_list);
     for (int64 i = 0; i < key_list_tmp.size(); ++i) {
-      V* val = value_ptr_list[i]->GetValue(emb_config_.emb_index, value_len_);
-      V* primary_val = value_ptr_list[i]->GetValue(emb_config_.primary_emb_index, value_len_);
+      V* val = value_ptr_list[i]->GetValue(emb_config_.emb_index, value_len_, kv_->GetOffset(emb_config_.emb_index));
+      V* primary_val = value_ptr_list[i]->GetValue(emb_config_.primary_emb_index, value_len_, kv_->GetOffset(emb_config_.emb_index));
       if (val != nullptr && primary_val != nullptr) {
         value_list->push_back(val);
         key_list->push_back(key_list_tmp[i]);
@@ -349,12 +357,11 @@ class EmbeddingVar : public ResourceBase {
         if (l2_weight < emb_config_.l2_weight_threshold) {
           to_deleted.push_back(std::pair<K, ValuePtr<V>*>(key_list[i], value_ptr_list[i]));
         }
-        value_ptr_list[i]->Free(val);
       }
       for (const auto it : to_deleted) {
         // TODO memory recycle
-        //(it.second)->Destroy(value_len_);
-        //delete it.second;
+        (it.second)->Destroy(alloc_, value_len_);
+        delete it.second;
         kv_->Remove(it.first);
       }
     return Status::OK();

tensorflow/core/framework/embedding/value_ptr.h
@@ -16,6 +16,7 @@ enum class LayoutType {
   LIGHT,
   NORMAL,
   LEVELDB,
+  NORMAL_FIX
 };
 
 namespace {
@@ -138,14 +139,60 @@ struct NormalHeader {
         freq_counter, freq_counter + count);
   }
 };
+
+struct FixLengthHeader {
+/*_________________________________________________________________________________
+  |                        |               |                embeddings             |
+  | slotflag + global step | freq counter  |                    V                  |
+  |                        |               |             actually value            |
+  |           int64        |     int64     |               by alloctor             |
+  |         (8 bytes)      |   (8 bytes)   |     (4 * slot_num * emb_dim bytes)    |
+  ---------------------------------------------------------------------------------
+*/
+  int64 global_step;
+  int64 freq_counter;
+
+  FixLengthHeader() {
+    memset(this, 0, sizeof(FixLengthHeader));
+  }
+
+   inline int64 GetGlobalStep() {
+    return global_step & 0x0000ffffffffffff;
+  }
+
+  inline void SetGlobalStep(int64 gs) {
+    int64 temp = global_step;
+    temp &= 0xffff000000000000;
+    temp |= gs;
+    global_step = temp;
+  }
+
+  inline int64 GetFreqCounter() {
+    return freq_counter;
+  }
+
+  inline void SetFreqCounter(int64 fc) {
+    freq_counter = fc;
+  }
+
+  inline void AddFreq() {
+    __sync_bool_compare_and_swap(&freq_counter,
+        freq_counter, freq_counter + 1);
+  }
+
+  inline void AddFreq(int64 count) {
+    __sync_bool_compare_and_swap(&freq_counter,
+        freq_counter, freq_counter + count);
+  }
+};
 } // namespace
 
 template <class V>
 class ValuePtr {
  public:
-  ~ValuePtr() {}
+  virtual ~ValuePtr() {}
 
-  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index) {
+  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index, int offset) {
     MetaHeader* meta = (MetaHeader*)ptr_;
     unsigned int embnum = (unsigned int)meta->embed_num;
     auto metadata = meta->GetColumnBitset();
@@ -173,7 +220,7 @@ class ValuePtr {
   }
 
   // simple getter for V* and version
-  virtual V* GetValue(int emb_index, int64 value_len) {
+  virtual V* GetValue(int emb_index, int64 value_len, int offset) {
     MetaHeader* meta = (MetaHeader*)ptr_;
     auto metadata = meta->GetColumnBitset();
     if (metadata.test(emb_index)) {
@@ -291,6 +338,77 @@ class NormalValuePtr : public ValuePtr<V> {
 };
 
 template <class V>
+class NormalContinousValuePtr : public ValuePtr<V>{
+  public:
+   NormalContinousValuePtr(size_t size) {
+    this->ptr_ = (void*)malloc(sizeof(FixLengthHeader) + sizeof(V) * size);
+    memset(this->ptr_ + sizeof(FixLengthHeader), 0, sizeof(V) * size);
+    new ((char*)this->ptr_) FixLengthHeader();
+   }
+  
+   ~NormalContinousValuePtr(){
+    free(this->ptr_);
+   }
+
+  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index, int offset) override {
+    int8 meta = *((int8*)((char*)this->ptr_ + 6));
+    std::bitset<8> bs(meta);
+    if (!bs.test(emb_index)) {
+      while(this->flag_.test_and_set(std::memory_order_acquire));
+      if (bs.test(emb_index)) {
+        return ((V*)this->ptr_ + sizeof(FixLengthHeader) / sizeof(V) + offset);
+      }
+      V* tensor_val = ((V*)this->ptr_ + sizeof(FixLengthHeader) / sizeof(V) + offset);
+      memcpy(tensor_val, default_v, sizeof(V) * value_len);
+      int8* m = (int8*)((char*)this->ptr_ + 6);
+      *m |= (1 <<  emb_index);
+      this->flag_.clear(std::memory_order_release);
+      return tensor_val;
+    } else {
+      return (V*)this->ptr_ + sizeof(FixLengthHeader) / sizeof(V) + offset;
+    }
+  }
+
+  virtual V* GetValue(int emb_index, int64 value_len, int offset) {
+    int8 meta = *((int8*)((char*)this->ptr_ + 6));
+    std::bitset<8> bs(meta);
+    if (bs.test(emb_index)) {
+      return ((V*)this->ptr_ + sizeof(FixLengthHeader) / sizeof(V) + offset);
+    } else {
+      return nullptr;
+    }
+  }
+
+  virtual void Destroy(Allocator* allocator, int64 value_len) {
+    return;
+  }
+  
+  int64 GetStep() {
+    return ((FixLengthHeader*)this->ptr_)->GetGlobalStep();
+  }
+
+  void SetStep(int64 gs) {
+    ((FixLengthHeader*)this->ptr_)->SetGlobalStep(gs);
+  }
+
+  int64 GetFreq() {
+    return ((FixLengthHeader*)this->ptr_)->GetFreqCounter();
+  }
+
+  void SetFreq(int64 freq) {
+    ((FixLengthHeader*)this->ptr_)->SetFreqCounter(freq);
+  }
+
+  void AddFreq() {
+    ((FixLengthHeader*)this->ptr_)->AddFreq();
+  }
+
+  void AddFreq(int64 count) {
+    ((FixLengthHeader*)this->ptr_)->AddFreq(count);
+  }
+};
+
+template <class V>
 class DBValuePtr : public NormalValuePtr<V> {
  public:
   DBValuePtr(size_t size, leveldb::DB* level_db) : NormalValuePtr<V>(size),
@@ -300,7 +418,7 @@ class DBValuePtr : public NormalValuePtr<V> {
   ~DBValuePtr() {
   }
 
-  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index) {
+  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index, int offset) {
     MetaHeader* meta = (MetaHeader*)this->ptr_;
     unsigned int embnum = (unsigned int)meta->embed_num;
     auto metadata = meta->GetColumnBitset();

DeepRec-AI/DeepRec

tensorflow/core/framework/embedding/embedding_var.h
@@ -100,6 +100,9 @@ class EmbeddingVar : public ResourceBase {
       default_value_ = TypedAllocator::Allocate<V>(alloc_, default_tensor.NumElements(), AllocationAttributes());
       auto default_tensor_flat = default_tensor.flat<V>();
       memcpy(default_value_, &default_tensor_flat(0), default_tensor.TotalBytes());
+      if (LayoutType::NORMAL_FIX == emb_config_.get_layout_type()) {
+        kv_->SetDim(emb_config_.emb_index, value_len_, emb_config_.slot_num + 1);
+      }
       return Status::OK();
     }
   }
@@ -131,6 +134,8 @@ class EmbeddingVar : public ResourceBase {
       }
       new_value_ptr_fn = [this] (size_t size) { return new DBValuePtr<V>(size, this->level_db_); };
      }
+    } else if (LayoutType::NORMAL_FIX == emb_config_.get_layout_type()){
+      new_value_ptr_fn = [] (size_t size) { return new NormalContiguousValuePtr<V>(size); };
     } else {
       return errors::InvalidArgument(name_, ", Unsupport EmbeddingVariable LayoutType.");
     }
@@ -163,6 +168,9 @@ class EmbeddingVar : public ResourceBase {
       default_value_ = TypedAllocator::Allocate<V>(alloc_, default_tensor.NumElements(), AllocationAttributes());
       auto default_tensor_flat = default_tensor.flat<V>();
       memcpy(default_value_, &default_tensor_flat(0), default_tensor.TotalBytes());
+      if (LayoutType::NORMAL_FIX == emb_config_.get_layout_type()) {
+        kv_->SetDim(emb_config_.emb_index, value_len_, emb_config_.slot_num + 1);
+      }
       return Status::OK();
     }
   }
@@ -180,7 +188,7 @@ class EmbeddingVar : public ResourceBase {
   }
 
   Status LookupOrCreateKey(K key, ValuePtr<V>** value_ptr, int64 update_version = -1) {
-    Status s = LookupOrCreateKeyInternal(key, value_ptr, emb_config_.total_num());
+    Status s = LookupOrCreateKeyInternal(key, value_ptr, emb_config_.total_num(kv_->GetTotalDims()));
     TF_CHECK_OK(s);
     if (emb_config_.is_primary() && emb_config_.steps_to_live != 0 && update_version != -1) {
       (*value_ptr)->SetStep(update_version);
@@ -210,11 +218,11 @@ class EmbeddingVar : public ResourceBase {
 
   V* LookupOrCreateEmb(ValuePtr<V>* value_ptr, const V* default_v) {
     return value_ptr->GetOrAllocate(alloc_, value_len_, default_v,
-        emb_config_.emb_index);
+        emb_config_.emb_index, kv_->GetOffset(emb_config_.emb_index));
   }
 
   V* LookupPrimaryEmb(ValuePtr<V>* value_ptr) {
-    V* primary_val = value_ptr->GetValue(emb_config_.primary_emb_index, value_len_);
+    V* primary_val = value_ptr->GetValue(emb_config_.primary_emb_index, value_len_, kv_->GetOffset(emb_config_.emb_index));
     return primary_val;
   }
 
@@ -297,8 +305,8 @@ class EmbeddingVar : public ResourceBase {
     std::vector<K> key_list_tmp;
     kv_->GetSnapshot(&key_list_tmp, &value_ptr_list);
     for (int64 i = 0; i < key_list_tmp.size(); ++i) {
-      V* val = value_ptr_list[i]->GetValue(emb_config_.emb_index, value_len_);
-      V* primary_val = value_ptr_list[i]->GetValue(emb_config_.primary_emb_index, value_len_);
+      V* val = value_ptr_list[i]->GetValue(emb_config_.emb_index, value_len_, kv_->GetOffset(emb_config_.emb_index));
+      V* primary_val = value_ptr_list[i]->GetValue(emb_config_.primary_emb_index, value_len_, kv_->GetOffset(emb_config_.emb_index));
       if (val != nullptr && primary_val != nullptr) {
         value_list->push_back(val);
         key_list->push_back(key_list_tmp[i]);
@@ -349,12 +357,11 @@ class EmbeddingVar : public ResourceBase {
         if (l2_weight < emb_config_.l2_weight_threshold) {
           to_deleted.push_back(std::pair<K, ValuePtr<V>*>(key_list[i], value_ptr_list[i]));
         }
-        value_ptr_list[i]->Free(val);
       }
       for (const auto it : to_deleted) {
         // TODO memory recycle
-        //(it.second)->Destroy(value_len_);
-        //delete it.second;
+        (it.second)->Destroy(alloc_, value_len_);
+        delete it.second;
         kv_->Remove(it.first);
       }
     return Status::OK();

tensorflow/core/framework/embedding/value_ptr.h
@@ -16,6 +16,7 @@ enum class LayoutType {
   LIGHT,
   NORMAL,
   LEVELDB,
+  NORMAL_FIX
 };
 
 namespace {
@@ -138,14 +139,60 @@ struct NormalHeader {
         freq_counter, freq_counter + count);
   }
 };
+
+struct FixedLengthHeader {
+/*_________________________________________________________________________________
+  |                        |               |                embeddings             |
+  | slotflag + global step | freq counter  |                    V                  |
+  |                        |               |             actually value            |
+  |           int64        |     int64     |               by alloctor             |
+  |         (8 bytes)      |   (8 bytes)   |     (4 * slot_num * emb_dim bytes)    |
+  ---------------------------------------------------------------------------------
+*/
+  int64 global_step;
+  int64 freq_counter;
+
+  FixedLengthHeader() {
+    memset(this, 0, sizeof(FixedLengthHeader));
+  }
+
+   inline int64 GetGlobalStep() {
+    return global_step & 0x0000ffffffffffff;
+  }
+
+  inline void SetGlobalStep(int64 gs) {
+    int64 temp = global_step;
+    temp &= 0xffff000000000000;
+    temp |= gs;
+    global_step = temp;
+  }
+
+  inline int64 GetFreqCounter() {
+    return freq_counter;
+  }
+
+  inline void SetFreqCounter(int64 fc) {
+    freq_counter = fc;
+  }
+
+  inline void AddFreq() {
+    __sync_bool_compare_and_swap(&freq_counter,
+        freq_counter, freq_counter + 1);
+  }
+
+  inline void AddFreq(int64 count) {
+    __sync_bool_compare_and_swap(&freq_counter,
+        freq_counter, freq_counter + count);
+  }
+};
 } // namespace
 
 template <class V>
 class ValuePtr {
  public:
-  ~ValuePtr() {}
+  virtual ~ValuePtr() {}
 
-  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index) {
+  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index, int offset) {
     MetaHeader* meta = (MetaHeader*)ptr_;
     unsigned int embnum = (unsigned int)meta->embed_num;
     auto metadata = meta->GetColumnBitset();
@@ -173,7 +220,7 @@ class ValuePtr {
   }
 
   // simple getter for V* and version
-  virtual V* GetValue(int emb_index, int64 value_len) {
+  virtual V* GetValue(int emb_index, int64 value_len, int offset) {
     MetaHeader* meta = (MetaHeader*)ptr_;
     auto metadata = meta->GetColumnBitset();
     if (metadata.test(emb_index)) {
@@ -291,6 +338,77 @@ class NormalValuePtr : public ValuePtr<V> {
 };
 
 template <class V>
+class NormalContiguousValuePtr : public ValuePtr<V>{
+  public:
+   NormalContiguousValuePtr(size_t size) {
+    this->ptr_ = (void*)malloc(sizeof(FixedLengthHeader) + sizeof(V) * size);
+    memset(this->ptr_ + sizeof(FixedLengthHeader), 0, sizeof(V) * size);
+    new ((char*)this->ptr_) FixedLengthHeader();
+   }
+  
+   ~NormalContiguousValuePtr(){
+    free(this->ptr_);
+   }
+
+  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index, int offset) override {
+    int8 meta = *((int8*)((char*)this->ptr_ + 6));
+    std::bitset<8> bs(meta);
+    if (!bs.test(emb_index)) {
+      while(this->flag_.test_and_set(std::memory_order_acquire));
+      if (bs.test(emb_index)) {
+        return ((V*)this->ptr_ + sizeof(FixedLengthHeader) / sizeof(V) + offset);
+      }
+      V* tensor_val = ((V*)this->ptr_ + sizeof(FixedLengthHeader) / sizeof(V) + offset);
+      memcpy(tensor_val, default_v, sizeof(V) * value_len);
+      int8* m = (int8*)((char*)this->ptr_ + 6);
+      *m |= (1 <<  emb_index);
+      this->flag_.clear(std::memory_order_release);
+      return tensor_val;
+    } else {
+      return (V*)this->ptr_ + sizeof(FixedLengthHeader) / sizeof(V) + offset;
+    }
+  }
+
+  virtual V* GetValue(int emb_index, int64 value_len, int offset) {
+    int8 meta = *((int8*)((char*)this->ptr_ + 6));
+    std::bitset<8> bs(meta);
+    if (bs.test(emb_index)) {
+      return ((V*)this->ptr_ + sizeof(FixedLengthHeader) / sizeof(V) + offset);
+    } else {
+      return nullptr;
+    }
+  }
+
+  virtual void Destroy(Allocator* allocator, int64 value_len) {
+    return;
+  }
+  
+  int64 GetStep() {
+    return ((FixedLengthHeader*)this->ptr_)->GetGlobalStep();
+  }
+
+  void SetStep(int64 gs) {
+    ((FixedLengthHeader*)this->ptr_)->SetGlobalStep(gs);
+  }
+
+  int64 GetFreq() {
+    return ((FixedLengthHeader*)this->ptr_)->GetFreqCounter();
+  }
+
+  void SetFreq(int64 freq) {
+    ((FixedLengthHeader*)this->ptr_)->SetFreqCounter(freq);
+  }
+
+  void AddFreq() {
+    ((FixedLengthHeader*)this->ptr_)->AddFreq();
+  }
+
+  void AddFreq(int64 count) {
+    ((FixedLengthHeader*)this->ptr_)->AddFreq(count);
+  }
+};
+
+template <class V>
 class DBValuePtr : public NormalValuePtr<V> {
  public:
   DBValuePtr(size_t size, leveldb::DB* level_db) : NormalValuePtr<V>(size),
@@ -300,7 +418,7 @@ class DBValuePtr : public NormalValuePtr<V> {
   ~DBValuePtr() {
   }
 
-  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index) {
+  virtual V* GetOrAllocate(Allocator* allocator, int64 value_len, const V* default_v, int emb_index, int offset) {
     MetaHeader* meta = (MetaHeader*)this->ptr_;
     unsigned int embnum = (unsigned int)meta->embed_num;
     auto metadata = meta->GetColumnBitset();