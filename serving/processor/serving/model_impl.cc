bf4f964d59aef862be87c416bc39ff82aba2d9ee
jiankeng.pt <jiankeng.pt@alibaba-inc.com>
2022-03-24 10:42:09 +0800

    [Serving] Refine serving code and fix build break.

---
+++ serving/processor/serving/model_impl.cc
@@ -0,0 +1,40 @@
+#include "serving/processor/serving/model_impl.h"
+#include "serving/processor/serving/model_config.h"
+#include "serving/processor/serving/model_instance.h"
+#include "serving/processor/serving/model_message.h"
+#include "tensorflow/core/framework/tensor.h"
+
+namespace tensorflow {
+namespace processor {
+SavedModelImpl::SavedModelImpl(ModelConfig* config) :
+  model_config_(config) {
+}
+
+SavedModelImpl::~SavedModelImpl() {
+  delete instance_mgr_;
+  delete model_config_;
+}
+
+Status SavedModelImpl::Init() {
+  instance_mgr_ = ModelInstanceMgrFactory::Create(model_config_);
+  return instance_mgr_->Init();
+}
+
+Status SavedModelImpl::Predict(Request& req, Response& resp) {
+  return instance_mgr_->Predict(req, resp);
+}
+
+Status SavedModelImpl::GetServingModelInfo(ServingModelInfo& model_info) {
+  return instance_mgr_->GetServingModelInfo(model_info);
+}
+
+Status SavedModelImpl::Rollback() {
+  return instance_mgr_->Rollback();
+}
+ 
+std::string SavedModelImpl::DebugString() {
+  return instance_mgr_->DebugString();
+}
+
+} // processor
+} // tensorflow
