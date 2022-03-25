2560a3850bea3ab955aa9090178e39d72c1db4ae
kushanam <kahmadian@nvidia.com>
2022-03-24 17:21:27 -0700

    [NV-TF] Fix layout optimizer test bug for arm.

--- tensorflow/core/grappler/optimizers/layout_optimizer_test.cc
+++ tensorflow/core/grappler/optimizers/layout_optimizer_test.cc
@@ -82,8 +82,9 @@ class LayoutOptimizerTest : public GrapplerTest {
         ops::Const(s->WithOpName("Filter"), Input::Initializer(filter_data));
 
     ops::Conv2D::Attrs attrs;
+    auto padding_values = gtl::ArraySlice<int>({0, 0, 1, 2, 3, 4, 0, 0});
     if (padding == "EXPLICIT") {
-      attrs = attrs.ExplicitPaddings({0, 0, 1, 2, 3, 4, 0, 0});
+      attrs = attrs.ExplicitPaddings(padding_values);
     }
 
     Output conv = ops::Conv2D(s->WithOpName("Conv2D").WithDevice(device), input,
