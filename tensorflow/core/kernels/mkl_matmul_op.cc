236aac1d2fe409844398080d03d4f643e268c49b
Changqing Li <changqing.li@intel.com>
2022-03-29 17:09:27 +0800

    [OP] Change fused matmul layout type and number thread for small size inputs. (#129)
    * [OP] Add env Marco for MKL ops intra thread setting.
    
    * [OP] Fix _MklMatMul op perf issue with small size inputs.
    
    * [OP] Change the layout type of _MklFusedMatMul op.
    
    Co-authored-by: marvin <weifei.yu@intel.com>

--- tensorflow/core/kernels/mkl_matmul_op.cc
+++ tensorflow/core/kernels/mkl_matmul_op.cc
@@ -160,9 +160,21 @@ class MklMatMulOp : public OpKernel {
     char char_transb = transb ? 'T' : 'N';
     VLOG(2) << "MKL DNN SGEMM called";
 #ifdef ENABLE_MKLDNN_THREADPOOL
-    MklDnnThreadPool eigen_tp(ctx);
-    sgemm(char_transa, char_transb, m, n, k, alpha, a, lda, b, ldb, beta, c,
-          ldc, &eigen_tp);
+    // With threadpool , the runtime overhead is comparable to the kernel
+    // execution for small kernel sizes. For such sizes, it may be better to run
+    // the kernel single threaded. Here we are coming up with a cost model based
+    // on L1 sizes. If we find that matrices are small enough, we will execute
+    // single threaded. This may need tuning.
+    if (ExecuteSingleThreadedGemm(m, n, k)) {
+      // For now, call single-threaded gemm.
+      MklDnnThreadPool eigen_tp(ctx, 1);
+      sgemm(char_transa, char_transb, m, n, k, alpha, a, lda, b, ldb,
+                    beta, c, ldc, &eigen_tp);
+    } else {
+      MklDnnThreadPool eigen_tp(ctx);
+      sgemm(char_transa, char_transb, m, n, k, alpha, a, lda, b, ldb,
+                    beta, c, ldc, &eigen_tp);
+    }
 #else
     dnnl_sgemm(char_transa, char_transb, m, n, k, alpha, a, lda, b, ldb, beta,
                c, ldc);
