b8ec418bd99af018ec90759c23d7242b838c8a50
Duyi-Wang <duyi.wang@intel.com>
2022-07-18 13:40:53 +0800

    [Op] Add fused ln forward compute and ut.

--- tensorflow/core/kernels/fused_layer_norm/fused_layer_normalize_ops_test.cc
+++ tensorflow/core/kernels/fused_layer_norm/fused_layer_normalize_ops_test.cc
@@ -19,7 +19,7 @@ enum class Device { CPU, GPU };
 class FusedLayerNormalizeOpTest : public OpsTestBase {
  protected:
   void MakeOpAndSetDevice(Device device, DataType dtype, int axis, float epsilon) {
-    TF_EXPECT_OK(NodeDefBuilder("fused_l2_normalize", "FusedLayerNorm")
+    TF_EXPECT_OK(NodeDefBuilder("fused_layer_normalize", "FusedLayerNorm")
                      .Attr("T", dtype)
                      .Attr("epsilon", epsilon)
                      .Input(FakeInput(DT_FLOAT))
@@ -31,19 +31,21 @@ class FusedLayerNormalizeOpTest : public OpsTestBase {
 };
 
 TEST_F(FusedLayerNormalizeOpTest, 2Dims_Float) {
-  const int rows = 4;
-  const int cols = 252; //128+64+32+16+8+4=252 1008
+  const int rows = 7;
+  const int cols = 255;
 
   MakeOpAndSetDevice(Device::CPU, DT_FLOAT, 0, 1e-12);
 
-  // x
-    float input_array[1008];
-    for (int i = 0; i < sizeof(input_array) / sizeof(float); i++) {
-      input_array[i] = 1.0;
-    }
+  float input_array[1785];
+  for (int i = 0; i < sizeof(input_array) / sizeof(float); i++) {
+    input_array[i] = 1.0;
+  }
+  for (int i =0; i < rows; i++) {
+    input_array[i * rows] = 2.0;
+  }
   AddInputFromArray<float>(TensorShape({rows, cols}), input_array);
-  AddInput<float>(TensorShape({cols}), [](int i) -> float { return 0.5; });
-  AddInput<float>(TensorShape({cols}), [](int i) -> float { return 0.5; });
+  AddInput<float>(TensorShape({cols}), [](int i) -> float { return 2.0; });
+  AddInput<float>(TensorShape({cols}), [](int i) -> float { return 1; });
 
   TF_ASSERT_OK(RunOpKernel());
   TF_EXPECT_OK(device_->Sync());
@@ -51,63 +53,83 @@ TEST_F(FusedLayerNormalizeOpTest, 2Dims_Float) {
   {
     Tensor expected_output(allocator(), DT_FLOAT,
                                 TensorShape({rows, cols}));
-    float output_array[1008];
+    Tensor mean(allocator(), DT_FLOAT, TensorShape({rows}));
+    Tensor rvariance(allocator(), DT_FLOAT, TensorShape({rows}));
+    float output_array[1785];
+    float result = -2.0f / sqrtf(254.0f) + 1;
     for (int i = 0; i < sizeof(output_array) / sizeof(float); i++) {
-      output_array[i] = 0.062994122505188;
+      output_array[i] = result;
+    }
+    for (int i =0; i < rows; i++) {
+      input_array[i * rows] = -2.0f * sqrtf(254.0f) + 1.0;
+    }
+
+    float mean_array[cols];
+    for (int i = 0; i < sizeof(mean_array) / sizeof(float); i++) {
+      mean_array[i] = 256 / 255;
+    }
+
+    float rvariance_array[cols];
+    for (int i = 0; i < sizeof(rvariance_array) / sizeof(float); i++) {
+      rvariance_array[i] = sqrtf(254.0f) / 255.0f;;
     }
     test::FillValues<float>(&expected_output, output_array);
+    test::FillValues<float>(&mean, mean_array);
+    test::FillValues<float>(&rvariance, rvariance_array);
     test::ExpectTensorNear<float>(expected_output, *GetOutput(0), 1e-6);
+    test::ExpectTensorNear<float>(mean, *GetOutput(1), 1e-6);
+    test::ExpectTensorNear<float>(rvariance, *GetOutput(2), 1e-6);
   }
 }
 
 //----------------------------------------------------------------------------//
 // Performance benchmarks                                                     //
 //----------------------------------------------------------------------------//
-static Graph* FusedLayerNormalize(int rows, int cols) {
-  Graph* g = new Graph(OpRegistry::Global());
-  DataType dtype = DT_FLOAT;
+// static Graph* FusedLayerNormalize(int rows, int cols) {
+//   Graph* g = new Graph(OpRegistry::Global());
+//   DataType dtype = DT_FLOAT;
 
-  Tensor in(dtype, TensorShape({rows, cols}));
-  in.flat<float>().setRandom();
-  Tensor gamma(dtype, TensorShape({cols}));
-  in.flat<float>().setRandom();
-  Tensor beta(dtype, TensorShape({cols}));
-  in.flat<float>().setRandom();
+//   Tensor in(dtype, TensorShape({rows, cols}));
+//   in.flat<float>().setRandom();
+//   Tensor gamma(dtype, TensorShape({cols}));
+//   in.flat<float>().setRandom();
+//   Tensor beta(dtype, TensorShape({cols}));
+//   in.flat<float>().setRandom();
 
-  Node* input_in = test::graph::Constant(g, in);
-  Node* input_gamma = test::graph::Constant(g, gamma);
-  Node* input_beta = test::graph::Constant(g, beta);
-  auto nodeBuilder = NodeBuilder(g->NewName("n"), "FusedLayerNorm")
-                    .Input(input_in)
-                    .Input(input_gamma)
-                    .Input(input_beta)
-                    .Attr("T", dtype)
-                    .Attr("epsilon", 1e-12);
-  TF_CHECK_OK(nodeBuilder.Finalize(g, nullptr));
+//   Node* input_in = test::graph::Constant(g, in);
+//   Node* input_gamma = test::graph::Constant(g, gamma);
+//   Node* input_beta = test::graph::Constant(g, beta);
+//   auto nodeBuilder = NodeBuilder(g->NewName("n"), "FusedLayerNorm")
+//                     .Input(input_in)
+//                     .Input(input_gamma)
+//                     .Input(input_beta)
+//                     .Attr("T", dtype)
+//                     .Attr("epsilon", 1e-12);
+//   TF_CHECK_OK(nodeBuilder.Finalize(g, nullptr));
 
-  return g;
-}
+//   return g;
+// }
 
-#define BM_FusedLayerNorm(ROWS, COLS, NTH)                                   \
-  static void BM_FusedLayerNorm##_##ROWS##_##COLS##_##NTH##_CPU(             \
-  int iters) {                                                               \
-  testing::UseRealTime();                                                    \
-  testing::ItemsProcessed(static_cast<int64>(iters) * ROWS * COLS * 3);      \
-  SessionOptions opts;                                                       \
-  opts.config.set_intra_op_parallelism_threads(NTH);                         \
-  test::Benchmark("cpu", FusedLayerNormalize(ROWS, COLS), &opts).Run(iters); \
-  }                                                                          \
-  BENCHMARK(BM_FusedLayerNorm##_##ROWS##_##COLS##_##NTH##_CPU);              \
+// #define BM_FusedLayerNorm(ROWS, COLS, NTH)                                   \
+//   static void BM_FusedLayerNorm##_##ROWS##_##COLS##_##NTH##_CPU(             \
+//   int iters) {                                                               \
+//   testing::UseRealTime();                                                    \
+//   testing::ItemsProcessed(static_cast<int64>(iters) * ROWS * COLS * 3);      \
+//   SessionOptions opts;                                                       \
+//   opts.config.set_intra_op_parallelism_threads(NTH);                         \
+//   test::Benchmark("cpu", FusedLayerNormalize(ROWS, COLS), &opts).Run(iters); \
+//   }                                                                          \
+//   BENCHMARK(BM_FusedLayerNorm##_##ROWS##_##COLS##_##NTH##_CPU);              \
 
-#define BM_FusedLayerNorm_NTH(ROWS, COLS) \
-  BM_FusedLayerNorm(ROWS, COLS, 1);       \
-  BM_FusedLayerNorm(ROWS, COLS, 4);       \
-  BM_FusedLayerNorm(ROWS, COLS, 8);       \
+// #define BM_FusedLayerNorm_NTH(ROWS, COLS) \
+//   BM_FusedLayerNorm(ROWS, COLS, 1);       \
+//   BM_FusedLayerNorm(ROWS, COLS, 4);       \
+//   BM_FusedLayerNorm(ROWS, COLS, 8);       \
 
-BM_FusedLayerNorm_NTH(1024, 63);
-BM_FusedLayerNorm_NTH(1024, 255);
-BM_FusedLayerNorm_NTH(1024, 511);
-BM_FusedLayerNorm_NTH(1024, 1023);
+// BM_FusedLayerNorm_NTH(1024, 63);
+// BM_FusedLayerNorm_NTH(1024, 255);
+// BM_FusedLayerNorm_NTH(1024, 511);
+// BM_FusedLayerNorm_NTH(1024, 1023);
 
 }
 }
