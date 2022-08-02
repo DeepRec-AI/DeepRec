3d7695330e5a7174a84ec1915918906f2641c765
Tongxuan Liu <tongxuan.ltx@alibaba-inc.com>
2022-08-02 15:50:54 +0800

    Revert "[TF1] Input Lib Test"
    This reverts commit 9c123724c1d07e0b48bf59d9673dc3d82f1ec2f7.

--- tensorflow/python/distribute/input_lib_test.py
+++ tensorflow/python/distribute/input_lib_test.py
@@ -764,20 +764,9 @@ class DistributedIteratorMultiWorkerTest(
             collective_all_reduce_strategy.CollectiveAllReduceStrategy):
           # Autosharded
           if task_id == 0:
-            if (context.num_gpus() == 1):
-              expected_values = [[[0, 1]], [[4]]]
-            else:
-              expected_values = [[[0, 1], [4]]]
-              expected_values[0].extend(
-                                      [[] for _ in range(context.num_gpus()-2)])
-
+            expected_values = [[[0, 1]], [[4]]]
           else:
-            if (context.num_gpus() == 1):
-              expected_values = [[[2, 3]], [[]]]
-            else:
-              expected_values = [[[2, 3], []]]
-              expected_values[0].extend(
-                                      [[] for _ in range(context.num_gpus()-2)])
+            expected_values = [[[2, 3]], [[]]]
 
           # input_context is for between-graph auto-sharding.
           input_context = distribute_lib.InputContext(
@@ -785,14 +774,7 @@ class DistributedIteratorMultiWorkerTest(
               input_pipeline_id=task_id,
               num_replicas_in_sync=2)
         else:
-          if (context.num_gpus() == 1):
-            expected_values = [[[0, 1]], [[2, 3]], [[4]]]
-          elif (context.num_gpus() == 2):
-            expected_values = [[[0, 1], [2, 3]], [[4], []]]
-          elif (context.num_gpus() >= 3):
-            expected_values = [[[0, 1], [2, 3], [4]]]
-            expected_values[0].extend([[] for _ in range(context.num_gpus()-3)])
-
+          expected_values = [[[0, 1]], [[2, 3]], [[4]]]
           input_context = None
 
         strategy.extended.experimental_enable_get_next_as_optional = True
