3a690d4b68e14ab74312d13ee25ceaedc2d5e390
FTT16 <zekun.feng@intel.com>
2022-09-07 16:23:21 +0800

    [ModelZoo]Update model's name in benchmark script config file.

--- modelzoo/benchmark/benchmark.sh
+++ modelzoo/benchmark/benchmark.sh
@@ -174,8 +174,8 @@ if [[ ! -f $config_file ]];then
 fi
 
 [[ $modelArgs == None ]] && modelArgs=
-[[ $cpus != None ]] && cpu_optional="--cpuset-cpus $cpus"
-[[ $gpus != None ]] && gpu_optional="--gpus $gpus"
+[[ $cpus != 'None' ]] && cpu_optional="--cpuset-cpus $cpus"
+[[ $gpus != 'None' ]] && gpu_optional="--gpus $gpus"
 [ ! -d $log_dir/$currentTime ] && mkdir -p "$log_dir/$currentTime"
 [ ! -d $checkpoint_dir/$currentTime ] && mkdir -p "$checkpoint_dir/$currentTime"
 
