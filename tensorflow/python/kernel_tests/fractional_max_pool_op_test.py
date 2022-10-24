53e5cccf1f351dd53b5efe4270b7a1ebbf1c7bd4
A. Unique TensorFlower <gardener@tensorflow.org>
2022-10-24 14:30:10 -0700

    [Op] Fix security vulnerability with FractionalMax(AVG)Pool with illegal pooling_ratio.
    Fix CVE-2022-41900, PiperOrigin-RevId: 483486453

--- tensorflow/python/kernel_tests/fractional_max_pool_op_test.py
+++ tensorflow/python/kernel_tests/fractional_max_pool_op_test.py
@@ -326,6 +326,23 @@ class FractionalMaxPoolTest(test.TestCase):
             name=None)
         self.evaluate(result)
 
+  def testPoolingRatioValueOutOfRange(self):
+    with self.cached_session() as _:
+      # Whether turn on `TF2_BEHAVIOR` generates different error messages
+      with self.assertRaisesRegex(
+          (errors.InvalidArgumentError, ValueError),
+          r"(pooling_ratio cannot be smaller than 1, got: .*)|(is negative)"):
+        result = nn_ops.gen_nn_ops.fractional_max_pool(
+            value=np.zeros([3, 30, 30, 3]),
+            pooling_ratio=[1, -1, 3, 1],
+            pseudo_random=False,
+            overlapping=False,
+            deterministic=False,
+            seed=0,
+            seed2=0,
+        )
+        self.evaluate(result)
+
 
 class FractionalMaxPoolGradTest(test.TestCase):
   """Tests for FractionalMaxPoolGrad.
