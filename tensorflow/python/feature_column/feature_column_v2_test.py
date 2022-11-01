70bd15ecdff8abd029887ccea58b6908d86e1b78
Chen Ding <candy.dc@alibaba-inc.com>
2022-11-02 01:36:14 +0800

    [Embedding] Fix build save graph bug when creating partitioned EmbeddingVariable in feature_column API. (#521)

--- tensorflow/python/feature_column/feature_column_v2_test.py
+++ tensorflow/python/feature_column/feature_column_v2_test.py
@@ -7671,6 +7671,26 @@ class EmbeddingColumnTest(test.TestCase):
           self.assertAlmostEqual(emb_r[i][j], emb_right[i][j])
    
   @test_util.run_deprecated_v1
+  def testEmbeddingVariableForSharedEmbeddingColumnsWithPartitionNum(self):
+    columns_list=[]
+    columns_list.append(fc.categorical_column_with_embedding("col_emb", dtype=dtypes.string, partition_num=4))
+    W = fc.shared_embedding_columns(columns_list,
+            dimension=3,
+            initializer=init_ops.ones_initializer(dtypes.float32),
+            shared_embedding_collection_name="xxxxx_shared")
+
+    ids={}
+    ids["col_emb"] = sparse_tensor.SparseTensor(indices=[[0,0],[1,0],[2,0],[3,0],[4,0]], values=["aaaa","bbbbb","ccc","4nn","5b"], dense_shape=[5, 5])
+    emb = fc_old.input_layer(ids, W)
+    fun = math_ops.multiply(emb, 2.0, name='multiply')
+    loss = math_ops.reduce_sum(fun, name='reduce_sum')
+    opt = ftrl.FtrlOptimizer(0.1, l1_regularization_strength=2.0, l2_regularization_strength=0.00001)
+    g_v = opt.compute_gradients(loss)
+    train_op = opt.apply_gradients(g_v)
+    init = variables_lib.global_variables_initializer()
+    saver = saver_module.Saver()
+
+  @test_util.run_deprecated_v1
   def test_transform_feature(self):
     a = fc.categorical_column_with_identity(key='aaa', num_buckets=3)
     a_embedded = fc.embedding_column(a, dimension=2)
