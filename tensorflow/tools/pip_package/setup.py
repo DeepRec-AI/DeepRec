bd7d5af05d1fc4ba37d99d3af000ba87e23bfb9e
Tongxuan Liu <tongxuan.ltx@alibaba-inc.com>
2023-01-24 15:07:24 +0800

    [Release] Update DeepRec release version to 1.15.5+deeprec2212.

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -47,7 +47,7 @@ DOCLINES = __doc__.split('\n')
 # result for pip.
 # Also update tensorflow/tensorflow.bzl and
 # tensorflow/core/public/version.h
-_VERSION = '1.15.5+deeprec2210'
+_VERSION = '1.15.5+deeprec2212'
 
 REQUIRED_PACKAGES = [
     'absl-py >= 0.9.0',
