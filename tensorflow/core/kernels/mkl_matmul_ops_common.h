0438c7d0f5967a108b95355a0039ba3f3417533f
Honglin Zhu <zhuhonglin.zhl@alibaba-inc.com>
2023-02-05 23:04:56 +0800

    [ARM] Update oneDNN and ACL.
        cherry-pick from TF2.x 59278. Update oneDNN to 2.7.3, Compute Library (ACL) to v22.11.

--- tensorflow/core/kernels/mkl_matmul_ops_common.h
+++ tensorflow/core/kernels/mkl_matmul_ops_common.h
@@ -61,9 +61,6 @@ struct MklDnnMatMulFwdParams {
   MEMORY_FORMAT dst_format;
   string dtypes = string("");
   bool const_weight;
-#ifdef DNNL_AARCH64_USE_ACL
-  void* weight_address = nullptr;
-#endif
   struct PostOpParam {
     string name;
     std::vector<float> param;
@@ -375,9 +372,6 @@ class MklDnnMatMulFwdPrimitiveFactory : public MklPrimitiveFactory<T> {
     key_creator.AddAsKey(dnnl_matmul_fwd_dims.dst_dims);
     key_creator.AddAsKey(dnnl_matmul_fwd_dims.dtypes);
     key_creator.AddAsKey(dnnl_matmul_fwd_dims.weight_format);
-#ifdef DNNL_AARCH64_USE_ACL
-    key_creator.AddAsKey(dnnl_matmul_fwd_dims.weight_address);
-#endif
 
     // Generate keys for post-ops
     for (auto const& post_op_param : dnnl_matmul_fwd_dims.post_op_params) {
@@ -555,9 +549,6 @@ struct MklMatMulParams {
   memory::dims a_strides;
   memory::dims b_strides;
   memory::dims c_strides;
-#ifdef DNNL_AARCH64_USE_ACL
-  int aarch64_counter;
-#endif
   struct PostOpParam {
     string name;
     std::vector<float> param;
@@ -609,6 +600,11 @@ class MklMatMulPrimitive : public MklPrimitive {
     context_.a_mem->set_data_handle(DummyData);
     context_.b_mem->set_data_handle(DummyData);
     context_.c_mem->set_data_handle(DummyData);
+  }
+
+  std::shared_ptr<dnnl::matmul::primitive_desc>
+  GetPrimitiveDesc() const {
+    return context_.prim_desc;
   }
 
  private:
@@ -751,9 +747,6 @@ class MklMatMulPrimitiveFactory : public MklPrimitiveFactory<T> {
     key_creator.AddAsKey(params.b_strides);
     key_creator.AddAsKey(params.c_strides);
     key_creator.AddAsKey(typeid(T).name());
-#ifdef DNNL_AARCH64_USE_ACL
-    key_creator.AddAsKey(params.aarch64_counter);
-#endif
 
     // Generate keys for post-ops
     for (auto const& post_op_param : params.post_op_params) {
