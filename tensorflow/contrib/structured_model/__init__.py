919711cd1243e5d068bb149502822bd232ead183
candy.dc <candy.dc@alibaba-inc.com>
2023-02-15 15:07:49 +0800

    Revert "[Runtime] Support Structured Model."
    This reverts commit 9cb1b9015fc7b54c1980f4d6a1251c317a2690b2.

--- tensorflow/contrib/structured_model/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tensorflow.contrib.structured_model.python.core import *
-from tensorflow.contrib.structured_model.python.utils import *
