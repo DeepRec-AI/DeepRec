d03a77fb00f6f995d02560a602b7d372bc30c8ca
Junqi Hu <silenceki@hotmail.com>
2023-02-25 11:30:34 +0800

    [Embedding] Fixed multiple usage of input_layer cause duplicated GroupLookup Node bug.

--- tensorflow/python/feature_column/group_embedding_column.py
+++ tensorflow/python/feature_column/group_embedding_column.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 
 _global_fusion_embedding_scope = []
 _group_id = 0
+_group_embedding_tensor = dict()
+_called = False
 
 def _global_group_embedding_scope_list():
     global _global_fusion_embedding_scope
@@ -12,6 +14,21 @@ def _global_group_embedding_scope_list():
 def _current_group_embedding_scope():
     global _global_fusion_embedding_scope
     return None if len(_global_fusion_embedding_scope) == 0 else _global_fusion_embedding_scope[-1]
+
+def _get_global_group_embedding_scope(builder=None,
+                                      weight_collections=None,
+                                      trainable=True):
+    global _group_embedding_tensor
+    global _global_fusion_embedding_scope
+    global _called
+    if not _called:
+        for fused_scope in _global_fusion_embedding_scope:
+            fused_output = fused_scope._get_dense_tensor(
+                builder, weight_collections, trainable)
+            for ec, output in zip(fused_scope.embedding_columns, fused_output):
+                _group_embedding_tensor[ec] = output
+        _called = True
+    return _group_embedding_tensor
 
 def _current_group_id():
     global _group_id
