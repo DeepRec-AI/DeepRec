16571e949435ee68d24caa36ddf00be0350c87b9
chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>
2023-03-03 16:42:45 +0800

    [SOK] Fix SOK compilation.

--- tensorflow/tools/pip_package/build_sok.sh
+++ tensorflow/tools/pip_package/build_sok.sh
@@ -4,15 +4,15 @@ if [[ -e tools/python_bin_path.sh ]]; then
 source tools/python_bin_path.sh
 fi
 
-HOROVOD_NCCL_LINK=SHARED HOROVOD_GPU_OPERATIONS=NCCL pip install horovod>=0.26.1
+HOROVOD_NCCL_LINK=SHARED HOROVOD_GPU_OPERATIONS=NCCL pip install 'horovod>=0.26.1'
 pip install scikit-build
 pip install cmake==3.21.1
 pip install twine
 
 export ENABLE_DEEPREC=ON
 export DeepRecWorkdir=`pwd`
-export DeepRecBuild=`pwd`/bazel-workdir
+export DeepRecBuild=`pwd`/bazel-DeepRec
 export MAKEFLAGS=-j$(nproc)
-cd ./bazel-workdir/external/hugectr/sparse_operation_kit
+cd ./bazel-DeepRec/external/hugectr/sparse_operation_kit
 
 "${PYTHON_BIN_PATH:-python}" setup.py install
