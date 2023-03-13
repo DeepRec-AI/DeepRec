f5e9c4ec17467c3b35b9e224d8c6d31490d554b1
JunqiHu <silenceki@hotmail.com>
2023-03-13 17:52:09 +0800

    [Op] Optimize TensorBufferPutGpuOp which is used in smart_stage.
     Tested by modelzoo: avg global_step improve 0.6 steps/s

--- tensorflow/core/kernels/tensor_buffer_ops.cc
+++ tensorflow/core/kernels/tensor_buffer_ops.cc
@@ -14,6 +14,13 @@ limitations under the License.
 ==============================================================================*/
 
 #include "tensorflow/core/kernels/tensor_buffer_ops.h"
+#include "tensorflow/core/util/work_sharder.h"
+
+#if GOOGLE_CUDA
+#include "tensorflow/core/common_runtime/gpu/gpu_device.h"
+#include "tensorflow/stream_executor/stream.h"
+#include "tensorflow/stream_executor/device_memory.h"
+#endif  // GOOGLE_CUDA
 
 namespace tensorflow {
 
@@ -91,9 +98,89 @@ class TensorBufferAsyncOp : public AsyncOpKernel {
   }
 };
 
-class TensorBufferPutOp : public TensorBufferOp {
+#ifdef GOOGLE_CUDA
+class TensorBufferPutGpuOp : public TensorBufferOp {
  public:
-  explicit TensorBufferPutOp(OpKernelConstruction* ctx) : TensorBufferOp(ctx) {
+  explicit TensorBufferPutGpuOp(OpKernelConstruction* ctx) : TensorBufferOp(ctx) {
+    OP_REQUIRES_OK(ctx, ctx->GetAttr("timeout_millis", &timeout_millis_));
+  }
+
+  inline size_t AlignBytes(size_t s) {
+#if EIGEN_MAX_ALIGN_BYTES == 0
+    return s;
+#else
+    return std::ceil(s * 1.0 / EIGEN_MAX_ALIGN_BYTES) * EIGEN_MAX_ALIGN_BYTES;
+#endif
+  }
+
+  void ComputeWithTensorBuf(OpKernelContext* ctx, TensorBuf* buf) override {
+    std::vector<Tensor> record;
+    record.reserve(ctx->num_inputs());
+
+    std::vector<int> input_offsets;
+    int total_bytes = 0;
+    int cost = 1000;
+    for (int i = 0; i < ctx->num_inputs(); ++i) {
+      auto& tensor_in = ctx->input(i);
+      input_offsets.push_back(total_bytes);
+      total_bytes += AlignBytes(tensor_in.TotalBytes());
+    }
+
+    Tensor fused_tensor;
+    //Allocate Pinned memory
+    AllocatorAttributes cpu_alloc_attr;
+    cpu_alloc_attr.set_on_host(true);
+    cpu_alloc_attr.set_gpu_compatible(true);
+    OP_REQUIRES_OK(ctx, ctx->allocate_temp(DT_INT8, {total_bytes},
+                    &fused_tensor, cpu_alloc_attr));
+    auto fused_tensor_data = const_cast<char*>(fused_tensor.tensor_data().data());
+
+    auto copy_task = [this, ctx, &input_offsets, &fused_tensor_data]
+           (int64 start, int64 end) {
+      for (auto i = start; i < end; ++i) {
+        const Tensor& input_tensor = ctx->input(i);
+        size_t tensor_bytes = input_tensor.TotalBytes();
+        std::copy_n(input_tensor.tensor_data().data(), tensor_bytes,
+                  fused_tensor_data + input_offsets[i]);
+      }
+    };
+    auto worker_threads = *(ctx->device()->tensorflow_cpu_worker_threads());
+    Shard(worker_threads.num_threads, worker_threads.workers, ctx->num_inputs(), cost, copy_task);
+
+    auto* d_context = static_cast<const GPUDeviceContext*>(ctx->op_device_context());
+    se::Stream* copy_stream = d_context->host_to_device_stream();
+    se::Stream* compute_stream = d_context->stream();
+    
+    Tensor gpu_tensor;
+    OP_REQUIRES_OK(ctx, ctx->allocate_temp(DT_INT8, {total_bytes}, &gpu_tensor));
+    
+    copy_stream->ThenWaitFor(compute_stream);
+    se::DeviceMemoryBase wrapped_dst(const_cast<char*>(gpu_tensor.tensor_data().data()),
+                                gpu_tensor.TotalBytes());
+    copy_stream->ThenMemcpy(&wrapped_dst, const_cast<char*>(fused_tensor_data),
+                            gpu_tensor.TotalBytes()).ok();
+    compute_stream->ThenWaitFor(copy_stream);
+    
+    for(size_t j = 0; j < ctx->num_inputs(); j++) {
+      size_t bytes_tensor_offset = input_offsets[j];
+      Tensor tensor_slice = gpu_tensor.Slice(bytes_tensor_offset,
+          bytes_tensor_offset + ctx->input(j).TotalBytes());
+      Tensor output(ctx->input(j).dtype());
+      OP_REQUIRES_OK(ctx, output.BitcastFrom(
+          tensor_slice, ctx->input(j).dtype(), ctx->input(j).shape()));
+      record.push_back(output);
+    }
+    ctx->SetStatus(buf->Put(record, timeout_millis_));
+  }
+
+ private:
+  int64 timeout_millis_;
+};
+#endif  // GOOGLE_CUDA
+
+class TensorBufferPutCpuOp : public TensorBufferOp {
+ public:
+  explicit TensorBufferPutCpuOp(OpKernelConstruction* ctx) : TensorBufferOp(ctx) {
     OP_REQUIRES_OK(ctx, ctx->GetAttr("timeout_millis", &timeout_millis_));
   }
 
@@ -111,11 +198,14 @@ class TensorBufferPutOp : public TensorBufferOp {
 };
 
 REGISTER_KERNEL_BUILDER(Name("TensorBufferPut").Device(DEVICE_CPU),
-                        TensorBufferPutOp);
+                        TensorBufferPutCpuOp);
 #if GOOGLE_CUDA
-REGISTER_KERNEL_BUILDER(Name("TensorBufferPut").Device(DEVICE_GPU),
-                        TensorBufferPutOp);
+REGISTER_KERNEL_BUILDER(Name("TensorBufferPut")
+                        .Device(DEVICE_GPU)
+                        .HostMemory("record"),
+                        TensorBufferPutGpuOp);
 #endif  // GOOGLE_CUDA
+
 #ifdef TENSORFLOW_USE_SYCL
 REGISTER_KERNEL_BUILDER(Name("TensorBufferPut").Device(DEVICE_SYCL),
                         TensorBufferPutOp);
