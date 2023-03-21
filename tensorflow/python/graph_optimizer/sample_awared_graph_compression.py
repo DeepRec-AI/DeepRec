5d950b3df55c6c84f70a90ba981e8b517032d815
candy.dc <candy.dc@alibaba-inc.com>
2023-03-21 15:32:08 +0800

    Revert "[GraphOptimizer] Support SparseTensor as placeholder in Sample-awared Graph Compression."
    This reverts commit 0159bbebd02759172e88b7a10ec11b86932fd6ed.

--- tensorflow/python/graph_optimizer/sample_awared_graph_compression.py
+++ tensorflow/python/graph_optimizer/sample_awared_graph_compression.py
@@ -8,7 +8,6 @@ import collections
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
-from tensorflow.python.framework import sparse_tensor
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import parsing_ops
 from tensorflow.python.ops import sparse_ops
@@ -30,7 +29,7 @@ def enable_sample_awared_graph_compression(user_tensors, item_tensors, item_size
 
 def find_boundery_tensors(user_ops, item_ops):
   queue_item = collections.deque()
-  queue_item.extend([x for x, _ in item_ops])
+  queue_item.extend(item_ops)
   queue_item_back = collections.deque()
   item_sets = set()
   processed_item = set()
@@ -64,19 +63,19 @@ def find_boundery_tensors(user_ops, item_ops):
 
   queue_user = collections.deque()
   queue_user.extend(user_ops)
-  user_sets = set([x for x, _ in user_ops])
+  user_sets = set()
   boundery_tensor_sets = set()
   while queue_user:
-    op, is_sparse_tensor = queue_user.popleft()
+    op = queue_user.popleft()
     for t in op.outputs:
       for op2 in t.consumers():
         if op2 in user_sets:
           continue
         if op2 in item_sets:
-          boundery_tensor_sets.add((t, is_sparse_tensor))
+          boundery_tensor_sets.add(t)
         else:
           user_sets.add(op2)
-          queue_user.append((op2, False if op2.type == "SparseCross" else is_sparse_tensor))
+          queue_user.append(op2)
   logging.info("[SampleAwaredGraphCompression] boundery_tensor_sets: %s", boundery_tensor_sets)
   return user_sets, item_sets, boundery_tensor_sets
 
@@ -88,30 +87,21 @@ def add_tile_op(boundery_tensor_sets, item_seq_length, user_sets, seq_mask_resha
   # so we need a masking. However, we do not need masking for serving as we
   # can safely assume the maximum size of the query items are item_seq_length.
   tiled_num = 0
-  for t, is_sparse_tensor in boundery_tensor_sets:
+  for t in boundery_tensor_sets:
     if not t.consumers():
       continue
     # only add tiles for those with batched tensors (dynamic shaped [?, ...])
     # as some constant operations, such as reshape, should not be tiled
     if len(t.get_shape().as_list()) > 0 and not t.get_shape().as_list()[0]:
       with ops.colocate_with(t.op):
-        if is_sparse_tensor:
-          range_tensor = math_ops.range(array_ops.squeeze(item_seq_length), dtype=dtypes.int64)
-          user_expand = array_ops.tile(t, array_ops.concat([item_seq_length, [1]], axis=0))
-          zero_tensor = array_ops.zeros_like(range_tensor, dtypes.int64)
-          stack = array_ops.stack([range_tensor, zero_tensor], axis=1)
-          repeat_tensor = array_ops.repeat(array_ops.expand_dims(array_ops.shape(t)[0], axis=0), item_seq_length, axis=0)
-          repeat_tensor = array_ops.repeat(stack, repeat_tensor, axis=0)
-          seq_user_input = repeat_tensor + user_expand
-        else:
-          user_expand = array_ops.expand_dims(t, 1)
-          tile_shape = array_ops.concat([[1], item_seq_length, [1 for i in range(len(t.get_shape()[1:]))]], axis=0)
-          user_tiled = array_ops.tile(user_expand, tile_shape)
-          reshape_shape = [-1]
-          if len(user_tiled.get_shape()) > 2:
-            user_tiled_shape = array_ops.shape(user_tiled)
-            reshape_shape = array_ops.concat([math_ops.reduce_prod(user_tiled_shape[:2],keepdims=True), user_tiled_shape[2:]], 0)
-          seq_user_input = array_ops.reshape(user_tiled, reshape_shape)
+        user_expand = array_ops.expand_dims(t, 1)
+        tile_shape = array_ops.concat([[1], item_seq_length, [1 for i in range(len(t.get_shape()[1:]))]], axis=0)
+        user_tiled = array_ops.tile(user_expand, tile_shape)
+        reshape_shape = [-1]
+        if len(user_tiled.get_shape()) > 2:
+          user_tiled_shape = array_ops.shape(user_tiled)
+          reshape_shape = array_ops.concat([math_ops.reduce_prod(user_tiled_shape[:2],keepdims=True), user_tiled_shape[2:]], 0)
+        seq_user_input = array_ops.reshape(user_tiled, reshape_shape)
         if seq_mask_reshaped:
           seq_user_input = array_ops.boolean_mask(seq_user_input, seq_mask_reshaped)
         t_ops = copy.copy(t.consumers())
@@ -126,7 +116,7 @@ def add_tile_op(boundery_tensor_sets, item_seq_length, user_sets, seq_mask_resha
   logging.info("[SampleAwaredGraphCompression] add %d TileOp", tiled_num)
 
 class SampleAwaredGraph(object):
-  # user_tensors: list of Tensor or SparseTensor
+  # user_tensors: list of common tensor
   # item_tensors: list of packed tensor
   def __init__(self, user_tensors, item_tensors, item_seq_length):
     self.user_tensors = user_tensors
@@ -175,21 +165,9 @@ class SampleAwaredGraph(object):
     user_ops = []
     item_ops = []
     for t in self.user_tensors:
-      if isinstance(t, ops.Tensor):
-        user_ops.append((t.op, False))
-      elif isinstance(t, sparse_tensor.SparseTensor):
-        user_ops.append((t.indices.op, True))
-        user_ops.append((t.values.op, False))
-      else:
-        raise ValueError("user_tensors mustbe Tensor or SparseTensor.")
+      user_ops.append(t.op)
     for t in self.item_tensors:
-      if isinstance(t, ops.Tensor):
-        item_ops.append((t.op, False))
-      elif isinstance(t, sparse_tensor.SparseTensor):
-        item_ops.append((t.indices.op, True))
-        item_ops.append((t.values.op, False))
-      else:
-        raise ValueError("item_tensors mustbe Tensor or SparseTensor.")
+      item_ops.append(t.op)
 
     # subGraph_1
     user_op_sets, _, boundery_tensor_sets = find_boundery_tensors(
