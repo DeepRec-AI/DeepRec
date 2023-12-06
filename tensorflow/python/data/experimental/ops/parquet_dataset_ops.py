00e497cf0ac0ccc402aeb81da8cc7ad31923877a
chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>
2023-12-06 10:11:36 +0800

    [IO] Fix tensor shape meta-data bug for DataFrame Value.
    Signed-off-by: chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>

--- tensorflow/python/data/experimental/ops/parquet_dataset_ops.py
+++ tensorflow/python/data/experimental/ops/parquet_dataset_ops.py
@@ -22,6 +22,7 @@ from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.data.ops import readers
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_shape
 from tensorflow.python.framework import tensor_spec
 from tensorflow.python.framework import type_spec
 from tensorflow.python.util import nest
@@ -108,12 +109,18 @@ class _ParquetDataset(dataset_ops.DatasetSource):  # pylint: disable=abstract-me
     self._batch_size = ops.convert_to_tensor(
       batch_size, dtype=dtypes.int64, name='batch_size')
     self._fields = fields
-    self._output_specs = {
-      f.name: (
-        DataFrameValueSpec(f)
-        if f.ragged_rank > 0
-        else tensor_spec.TensorSpec(shape=[None], dtype=f.dtype))
-      for f in self._fields}
+    self._output_specs = {}
+    for f in self._fields:
+      item = None
+      if f.ragged_rank > 0:
+        item = DataFrameValueSpec(f)
+      else:
+        shape = tensor_shape.vector(batch_size if drop_remainder else None)
+        if f.shape:
+          shape = shape.concatenate(f.shape)
+        item = tensor_spec.TensorSpec(shape=shape, dtype=f.dtype)
+      self._output_specs[f.name] = item
+
     self._field_names = nest.flatten({f.name: f.name for f in self._fields})
     self._field_dtypes = nest.flatten({f.name: f.dtype for f in self._fields})
     self._field_ragged_ranks = nest.flatten(
